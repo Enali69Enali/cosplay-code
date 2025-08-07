d1ea57fba8baf942bbd5cb583a25e7bb259487e9
Enali69Enali <enali.ilane9@gmail.com>
2025-08-07 14:23:22 +0200

    Bouddha_project [2025]: Create a setup function.
    Signed-off-by: Enali69Enali <enali.ilane9@gmail.com>

--- project/bouddha/main.c
+++ project/bouddha/main.c
@@ -1,7 +1,7 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-int main(void) {
+void setup(){
     // Motor configuration 
     // Set PD3 (digital pin 3) as output to control the motor
     DDRD |= (1 << PD3);      // DDRD: Data Direction Register for Port D :1 = OUTPUT
@@ -10,7 +10,11 @@ int main(void) {
     // Set PD2 (digital pin 2) as input with internal pull-up resistor
     DDRD &= ~(1 << PD2);     // Clear bit = input mode : 0 = INPUT
     PORTD |= (1 << PD2);     // Enable pull-up resistor
+}
 
+int main(void) {
+    setup();
+    
     while(1) {
         // If button is pressed (logic LOW on PD2)
         if(!(PIND & (1 << PD2))) {
